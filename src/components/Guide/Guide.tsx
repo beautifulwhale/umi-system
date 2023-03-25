c966ee19ce20dc60b7ca67ff842aa9e2e6605e25
lyh <1928999500@qq.com>
2023-03-25 20:22:12 +0800

    feat: init project

---
+++ src/components/Guide/Guide.tsx
@@ -0,0 +1,25 @@
+import { Layout, Row, Typography } from 'antd';
+import React, { useEffect } from 'react';
+import styles from './Guide.less';
+
+interface Props {
+  name: string;
+}
+// 脚手架示例组件
+const Guide: React.FC<Props> = (props) => {
+  const { name } = props;
+  useEffect(() => {
+    console.log(process.env.UMI_ENV);
+  }, []);
+  return (
+    <Layout>
+      <Row>
+        <Typography.Title level={3} className={styles.title}>
+          欢迎使用 <strong>{name}</strong> ！
+        </Typography.Title>
+      </Row>
+    </Layout>
+  );
+};
+
+export default Guide;
